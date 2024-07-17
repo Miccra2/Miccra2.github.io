ae1b0cb851e9f4ed8023aa024fc70d9d60e0b9f1
Miccra2 <miccra@miccras-laptop.speedport.ip>
2024-07-17 20:57:59 +0200

    processor and tests

--- emulators/6502/tests.js
+++ emulators/6502/tests.js
@@ -1,8 +1,24 @@
+class Test {
+    mem;
+    cpu;
+
+    constructor() {
+        const MEMORYSIZE = 0x10000;
+        this.mem = new Memory(MEMORYSIZE);
+        this.cpu = new Processor();
+    }
+
+    TestInitializationAndReset() {
+        this.mem.Initialize();
+        this.cpu.Initialize();
+    }
+}
+
 function test() {
     const MEMORYSIZE = 0x10000;
     const MEM = new Memory(MEMORYSIZE);
     const CPU = new Processor();
 
     MEM.Initialize();
-    CPU.Reset();
+    CPU.Reset(MEM);
 }
